ff87ba56c9e01de547e9b858c1587ae780cda990
Nicholas A Christensen <nickachristensen@gmail.com>
2022-02-08 17:41:01 -0500

    commit tree 9dc828faf555c4a3de32572cf5c5dd1cda57af21

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(seasons) {
+    for (const banana of seasons) {
+        if (banana.result === "W") {
+            return banana.year;
+        }
+    }
+}
 
+record.find(superBowlWin)
